1a3aeb7d24427e952756a65ac5c12f5b335322f0
Jacob Stenzel <jkstenzel95@outlook.com>
2021-12-02 00:32:05 -0800

    Server-Operator initial Dockerfile

---
+++ server-operator/scripts/start.sh
@@ -0,0 +1,8 @@
+# Game specific setup script
+./${GAME_NAME}/setup.sh
+
+# Game specific setup script
+./${GAME_NAME}/start_server.sh
+
+# Start the server-operator here
+./start_server_operator.sh
---
+++ server-operator/scripts/start_server_operator.sh
@@ -0,0 +1,1 @@
+# Code for running the .NET server-operator service
