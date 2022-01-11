79323c3894373739c61477b17736bc260834fa6f
jkstenzel95 <jkstenzel95@JKS-Zephyrus-G14.localdomain>
2022-01-11 15:42:32 -0500

    Created access policy against synamodb

--- shared/scripts/init/Ark/backup.sh
+++ shared/scripts/init/Ark/backup.sh
@@ -2,5 +2,5 @@
  
 if ! pgrep -x "ShooterGameServer" > /dev/null
 then
-    python3 ./Ark/backup.py --server-mount-location $SERVER_MOUNT_LOCATION --backup-storage-url $BACKUP_STORAGE_NAME
+    python3 ./Ark/backup.py --shared-mount-location $SERVER_MOUNT_LOCATION --backup-storage-url $BACKUP_STORAGE_NAME
 fi
