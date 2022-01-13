af737062fa489597ef70e57d863926afd8b95175
Jacob Stenzel <jkstenzel95@outlook.com>
2022-01-13 15:31:21 -0800

    activated revised scripts on launch

---
+++ shared/scripts/init/get-backup-version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# TODO
+echo "{ \"Key\": { \"S\": \"${GAME_NAME}_${MAP_NAME}_backup_version\" } }" > key.json
+aws dynamodb get-item --table-name "jks-gs-${ENV}-${REGION_SHORTNAME}-${GAME_NAME}-${MAP_NAME}-kv_table" --key "file://key.json" --projection-expression "KeyValue" | jq '."Item"."KeyValue"."S"' | tr -d '"'
