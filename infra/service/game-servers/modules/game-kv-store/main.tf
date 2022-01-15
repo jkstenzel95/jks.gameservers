a5f27d0c4efb8421f995f6786d0af4178b6a3040
Jacob Stenzel <jkstenzel95@outlook.com>
2022-01-14 20:13:23 -0800

    Don't destroy kv

--- infra/service/game-servers/modules/game-kv-store/main.tf
+++ infra/service/game-servers/modules/game-kv-store/main.tf
@@ -2,6 +2,9 @@ resource "aws_dynamodb_table" "game_key_value_store" {
   name             = "jks-gs-${var.env}-${var.region_shortname}-${var.game_name}-${var.map_name}-kv_table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key         = "Key"
+  lifecycle {
+    prevent_destroy = true
+  }
 
   attribute {
     name = "Key"
