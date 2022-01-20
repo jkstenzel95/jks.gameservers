6d3581552d1a6a7fec63bce7f33b32a2e6bd6bb2
jkstenzel95 <jkstenzel95@JKS-Zephyrus-G14.localdomain>
2022-01-19 23:01:13 -0800

    Fix password variable assignment

--- shared/scripts/runtime/Ark/start-map.sh
+++ shared/scripts/runtime/Ark/start-map.sh
@@ -24,7 +24,7 @@ fi
 
 SessionName="${prefix}${MAP_NAME}${postfix}"
 
-SERVER_ADMIN_PASSWORD = aws secretsmanager get-secret-value --secret-id jks/gameservers/${ENVIRONMENT}/${REGION_SHORTNAME}/Ark-Server-Admin-Password --query SecretString --output text | jq '."Ark-Server-Admin-Password"' | tr -d '"'
+SERVER_ADMIN_PASSWORD=$(aws secretsmanager get-secret-value --secret-id jks/gameservers/${ENVIRONMENT}/${REGION_SHORTNAME}/Ark-Server-Admin-Password --query SecretString --output text | jq '."Ark-Server-Admin-Password"' | tr -d '"')
 
 $SERVER_MOUNT_LOCATION/Ark/ShooterGame/Binaries/Linux/ShooterGameServer "${MAP_CODE}?listen?Multihome=0.0.0.0?SessionName=${SessionName}?MaxPlayers=${MAX_PLAYERS}?QueryPort=${QUERY_PORT}?RCONPort=${RCON_PORT}?Port=${SERVER_PORT_1}?ServerAdminPassword=${SERVER_ADMIN_PASSWORD}?AltSaveDirectoryName=${MAP_NAME}?OverrideOfficialDifficulty=5.0?GameModIds=${MOD_LIST}${ADDITIONAL_SERVER_PARAMS}" -server -log -NoTransferFromFiltering -exclusivejoin -clusterid="jks_cluster"
 
