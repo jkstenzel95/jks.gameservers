e2facea9e8373198544f31710cbdbb09a97c9a64
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-05 12:57:29 -0800

    Minecraft chart deploy script changes

--- game-server/scripts/deploy_minecraft_chart.py
+++ game-server/scripts/deploy_minecraft_chart.py
@@ -22,28 +22,27 @@ def apply_charts(mappings_file, config_file, env, test):
                 if game["name"] == "Minecraft":
                     map_set = set(game["maps"])
                     image_version = game["image_version"]
-                    for idx, map_info in enumerate(mappings_json["maps"]):
-                        if map_info["name"] in map_set:
-                            print("Creating cluster for map {}; Image tag {}".format(map_info["name"], map_info["additional_server_params"], image_version))
-                            game_port = deployment_utilities.get_port_number(25565, idx)
-                            rcon_port = game_port + 1
-                            print("We're looking at game port {} with an RCON port of {}".format(game_port, rcon_port))
-                            gp_port_string = "ports[0].name=SERVER_PORT,ports[0].protocol=TCP,ports[0].number={},ports[0].game=Minecraft,ports[0].map={}".format(game_port, map_info["name"])
-                            rcon_port_string = "ports[1].name=RCON_PORT,ports[1].protocol=UDP,ports[1].number={},ports[1].game=Minecraft,ports[1].map={}".format(rcon_port, map_info["name"])
-                            port_name_prefix = "MINECRAFT_{}_{}".format(map_info["name"].upper(), env.upper())
-                            ports.append({ "name": "{}_SERVER_PORT".format(port_name_prefix), "protocol": "TCP", "number": game_port, "game": "Minecraft", "map": map_info["name"] })
-                            ports.append({ "name": "{}_RCON_PORT".format(port_name_prefix), "protocol": "UDP", "number": rcon_port, "game": "Minecraft", "map": map_info["name"] })
-                            env_file = "{}_env_list.txt".format(map_info["name"])
-                            env_file_path = "{}/../helm/game-server/{}".format(dir_path, env_file)
-                            # No justification for a additional env variables yet/anymore. Here as a guideline to show how it's done, but has no effect on the deployment
-                            env_dict = {  }
-                            deployment_utilities.generate_env_file(env_dict, env_file_path)
-                            values_string = "--set imageTag={},game=Minecraft,map={},environmentVariableFile={},{},{}".format(image_version, map_info["name"], env_file, gp_port_string, rcon_port_string)
-                            call_command = ["{}/helm_deploy.sh".format(dir_path), "-g", "Minecraft", "-m", map_info["name"],"-e", env, "-v", values_string]
-                            if test:
-                                call_command.append("-t")
-                            call(call_command)
-                            os.remove(env_file_path)
+                    for idx, map in enumerate(map_set):
+                        print("Creating cluster for map {}; Image tag {}".format(map, image_version))
+                        game_port = deployment_utilities.get_port_number(25565, idx)
+                        rcon_port = game_port + 1
+                        print("We're looking at game port {} with an RCON port of {}".format(game_port, rcon_port))
+                        gp_port_string = "ports[0].name=SERVER_PORT,ports[0].protocol=TCP,ports[0].number={},ports[0].game=Minecraft,ports[0].map={}".format(game_port, map)
+                        rcon_port_string = "ports[1].name=RCON_PORT,ports[1].protocol=UDP,ports[1].number={},ports[1].game=Minecraft,ports[1].map={}".format(rcon_port, map)
+                        port_name_prefix = "MINECRAFT_{}_{}".format(map.upper(), env.upper())
+                        ports.append({ "name": "{}_SERVER_PORT".format(port_name_prefix), "protocol": "TCP", "number": game_port, "game": "Minecraft", "map": map })
+                        ports.append({ "name": "{}_RCON_PORT".format(port_name_prefix), "protocol": "UDP", "number": rcon_port, "game": "Minecraft", "map": map })
+                        env_file = "{}_env_list.txt".format(map)
+                        env_file_path = "{}/../helm/game-server/{}".format(dir_path, env_file)
+                        # No justification for a additional env variables yet/anymore. Here as a guideline to show how it's done, but has no effect on the deployment
+                        env_dict = {  }
+                        deployment_utilities.generate_env_file(env_dict, env_file_path)
+                        values_string = "--set imageTag={},game=Minecraft,map={},environmentVariableFile={},{},{}".format(image_version, map, env_file, gp_port_string, rcon_port_string)
+                        call_command = ["{}/helm_deploy.sh".format(dir_path), "-g", "Minecraft", "-m", map,"-e", env, "-v", values_string]
+                        if test:
+                            call_command.append("-t")
+                        call(call_command)
+                        os.remove(env_file_path)
     return ports
 
 if __name__ == '__main__':
