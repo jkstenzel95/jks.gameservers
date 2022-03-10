1bd5f207150254475953394f35bd4e235cf45049
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-09 23:44:04 -0800

    TAdd testing sleep line while I poke arround

--- shared/scripts/runtime/Minecraft/start-map.sh
+++ shared/scripts/runtime/Minecraft/start-map.sh
@@ -13,3 +13,4 @@ done
 # TODO: the game server runtime image may need to reinstall dependencies. In fact, I think most will. Fix structuring.
 . "${SHARED_DIR}/shared/scripts/init/Minecraft/install-dependencies.sh"
 java -Xmx1024M -Xms1024M -jar $SERVER_MOUNT_LOCATION/Minecraft/server.jar
+sleep 3600
