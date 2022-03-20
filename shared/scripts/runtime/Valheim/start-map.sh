5977361485854b2723114843c952fe7f3b2a623e
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-20 16:58:16 -0700

    Fix directory name bug that prevented Valheim from launching in install directory

--- shared/scripts/runtime/Valheim/start-map.sh
+++ shared/scripts/runtime/Valheim/start-map.sh
@@ -12,7 +12,7 @@ do
     esac
 done
 
-pushd "${SERVER_MOUNT_LOCATION}/Ark"
+pushd "${SERVER_MOUNT_LOCATION}/Valheim"
 
 if [[ "${prefix}" != "" ]]; then
     prefix="${prefix} :: "
@@ -24,14 +24,15 @@ fi
 
 SessionName="${prefix}${MAP_NAME}${postfix}"
 config_file_name="${SHARED_DIR}/shared/config/${ENVIRONMENT}.json"
-mappings_file_name="${SHARED_DIR}/shared/data/ark_mappings.json"
+mappings_file_name="${SHARED_DIR}/shared/data/valheim_mappings.json"
 MAP_DATA=$(cat $mappings_file_name | jq ".maps[] | select(.name == \"$MAP_NAME\")")
 CONFIG_DATA=$(cat $config_file_name | jq ".games[] | select(.name == \"$GAME_NAME\")")
-MOD_LIST=$(echo $CONFIG_DATA | jq ".mods | @csv" | tr -d '"' | tr -d " ")
-ADDITIONAL_SERVER_PARAMS=$(echo $MAP_DATA | jq ".additional_server_params" | tr -d '"')
 
-export templdpath=$LD_LIBRARY_PATH  
+echo "LD_LIBRARY_PATH preswap: $LD_LIBRARY_PATH"
+export templdpath=$LD_LIBRARY_PATH
+echo "templdpath preswap: $LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH=$SERVER_MOUNT_LOCATION/Valheim/linux64:$LD_LIBRARY_PATH  
+echo "LD_LIBRARY_PATH postswap: $LD_LIBRARY_PATH"
 export SteamAppID=892970
 
 SERVER_PASSWORD=$(cat /mnt/secrets-store/jks_gameservers_${ENVIRONMENT}_${REGION_SHORTNAME}_Valheim-Server-Password | jq '."Valheim-Server-Password"' | tr -d '"')
