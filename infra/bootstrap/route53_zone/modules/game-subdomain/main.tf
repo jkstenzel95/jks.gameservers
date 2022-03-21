33fab1f5c089916a68d7bcb5404a02e347abdde4
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-20 18:13:27 -0700

    Add volume IDs for new Minecraft maps

--- infra/bootstrap/route53_zone/modules/game-subdomain/main.tf
+++ infra/bootstrap/route53_zone/modules/game-subdomain/main.tf
@@ -1,8 +1,8 @@
 locals {
     ark_mapsets = [ "all" ]
     minecraft_maps = [ "main", "greg", "greedy", "rebirth" ]
-    valheim_maps = [ "Audigr" ]
-    maps = var.game_name == "ark" ? local.ark_mapsets : ( var.game_name == "minecraft" ? local.minecraft_maps : [] )
+    valheim_maps = [ "audigr" ]
+    maps = var.game_name == "ark" ? local.ark_mapsets : ( var.game_name == "mc" ? local.minecraft_maps : (var.game_name == "valheim" ? local.valheim_maps : [] ) )
     domain = lower("${var.game_name}.${var.domain_name}")
 }
 
