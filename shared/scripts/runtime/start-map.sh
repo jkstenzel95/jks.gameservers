5a7a9e63bcc3dc75bb8d98d617d15abd53ce1f2b
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-20 23:25:17 -0700

    Fix syntax error in start map script

--- shared/scripts/runtime/start-map.sh
+++ shared/scripts/runtime/start-map.sh
@@ -1,3 +1,4 @@
 if [ $NEEDS_SETUP == "true" ]; then
     . "${SHARED_DIR}/shared/scripts/init/${GAME_NAME}/install-dependencies.sh"
+fi
 . "${SHARED_DIR}/shared/scripts/runtime/${GAME_NAME}/start-map.sh" -e "jks"
