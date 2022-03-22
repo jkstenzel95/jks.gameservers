e5aa52ac06ac82f762c04f52bbfb789de5a2f6e3
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-21 23:10:52 -0700

    Fix Minecraft forge logic

--- shared/scripts/init/Minecraft/base-install.sh
+++ shared/scripts/init/Minecraft/base-install.sh
@@ -6,13 +6,13 @@ pushd $scripts_dir
 
 mkdir "${SERVER_MOUNT_LOCATION}/Minecraft"
 
+mappings_file_name="${SHARED_DIR}/shared/data/minecraft_mappings.json"
+is_modded=$(cat $mappings_file_name | jq ".maps[] | select(.name == \"$MAP_NAME\") | .is_modded" | tr -d '"')
+
 if [ "${is_modded}" == "true" ]; then
     bash "${scripts_dir}/init/Minecraft/install-forge.sh"
     bash "${scripts_dir}/init/Minecraft/apply-forge-modpack.sh"
 else
-
-mappings_file_name="${SHARED_DIR}/shared/data/minecraft_mappings.json"
-is_modded=$(cat $mappings_file_name | jq ".maps[] | select(.name == \"$MAP_NAME\") | .is_modded" | tr -d '"')
 
 bash "${scripts_dir}/runtime/Minecraft/start-map.sh"
 
--- shared/scripts/init/Minecraft/post-install.sh
+++ shared/scripts/init/Minecraft/post-install.sh
@@ -3,7 +3,7 @@
 # TODO_MOD: This doesn't need to happen. In fact server.properties should be copied down.
 pushd "${SERVER_MOUNT_LOCATION}"
 
-if [ "${is_modded}" == "true" ]; then
+if [ "${is_modded}" == "false" ]; then
     download_url=$(curl --no-progress-meter "$(curl --no-progress-meter https://launchermeta.mojang.com/mc/game/version_manifest.json | jq ".versions[0].url" | tr -d "\"")" | jq ".downloads.server.url" |  tr -d "\"")
     curl $download_url --output ./Minecraft/server.jar
 fi
