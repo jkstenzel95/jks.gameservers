c070ac1cfa9df0f7e3cd949242661e676efbf1b7
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-23 16:57:47 -0700

    Actually download modpack

--- shared/scripts/init/Minecraft/apply-forge-modpack.sh
+++ shared/scripts/init/Minecraft/apply-forge-modpack.sh
@@ -4,6 +4,7 @@ pushd "${SERVER_MOUNT_LOCATION}/Minecraft"
 
 mappings_file_name="${SHARED_DIR}/shared/data/minecraft_mappings.json"
 modpack_zip=$(cat $mappings_file_name | jq ".maps[] | select(.name == \"$MAP_NAME\") | .modpack_zip" | tr -d '"')
+aws s3 cp "s3://${RESOURCE_BUCKET_NAME}/$modpack_zip" "./$modpack_zip"
 unzip -o $modpack_zip
 rm $modpack_zip
 
