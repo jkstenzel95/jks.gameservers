8a1a934d563f91ee2f2096e5922af80bab5b9a35
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-23 15:54:00 -0700

    Retain Minecraft mod files when restoring backup

--- shared/scripts/init/Minecraft/restore-backup.sh
+++ shared/scripts/init/Minecraft/restore-backup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 aws s3 cp "s3://${BACKUP_STORAGE_NAME}/${backup_version}.zip" Restore.zip
-mv "${SERVER_MOUNT_LOCATION}/Minecraft" "${SERVER_MOUNT_LOCATION}/Minecraft_old"
+
+if [ -d "${SERVER_MOUNT_LOCATION}/Minecraft/world" ]; then
+    mv "${SERVER_MOUNT_LOCATION}/Minecraft/world" "${SERVER_MOUNT_LOCATION}/Minecraft/world_old"
+fi
+
 if unzip -o Restore.zip -d "${SERVER_MOUNT_LOCATION}/Minecraft" ; then
-    rm -R "${SERVER_MOUNT_LOCATION}/Minecraft_old"
+    if [ -d "${SERVER_MOUNT_LOCATION}/Minecraft/world_old" ]; then
+        rm -R "${SERVER_MOUNT_LOCATION}/Minecraft/world_old"
+    fi
 else
-    mv "${SERVER_MOUNT_LOCATION}/Minecraft_old" "${SERVER_MOUNT_LOCATION}/Minecraft"
+    if [ -d ${SERVER_MOUNT_LOCATION}/Minecraft/world_old ]; then
+        mv "${SERVER_MOUNT_LOCATION}/Minecraft/world_old" "${SERVER_MOUNT_LOCATION}/Minecraft/world"
+    fi
     echo "Backup restore failed! Reverting to last."
 fi
 
