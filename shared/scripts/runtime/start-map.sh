55fe948c98170ded47a7195682fa9e5fabd5906b
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-23 23:16:24 -0700

    Hang minecraft server for investigation

--- shared/scripts/runtime/start-map.sh
+++ shared/scripts/runtime/start-map.sh
@@ -2,3 +2,6 @@ if [ $NEEDS_SETUP == "true" ]; then
     . "${SHARED_DIR}/shared/scripts/init/${GAME_NAME}/install-dependencies.sh"
 fi
 . "${SHARED_DIR}/shared/scripts/runtime/${GAME_NAME}/start-map.sh" -e "jks"
+
+echo "Server crash! Pausing for investigation"
+sleep 1800
