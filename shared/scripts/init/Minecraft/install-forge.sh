5f69c7df83002dc22ded5045e19462bed321784c
Jacob Stenzel <jkstenzel95@outlook.com>
2022-03-27 22:43:35 -0700

    Try upgrade of server version

--- shared/scripts/init/Minecraft/install-forge.sh
+++ shared/scripts/init/Minecraft/install-forge.sh
@@ -10,10 +10,11 @@ if [ -f /etc/profile.d/jdk.sh ]; then
 fi
 
 if [ ! -f "${forge_version}-universal.jar" ]; then
-    echo "Universal jar already exists, skipping installer download"
     aws s3 cp s3://${RESOURCE_BUCKET_NAME}/${forge_version}-installer.jar "./${forge_version}-installer.jar"
     java -jar "${forge_version}-installer.jar" --installServer
     rm ${forge_version}-installer.jar ${forge_version}-installer.jar.log
+else
+    echo "Universal jar already exists, skipping installer download"
 fi
 
 popd
