caaa487988d6c224f357e3922fc9d7d7421f3d2f
Jacob Stenzel <jkstenzel95@outlook.com>
2022-04-08 17:17:36 -0700

    Handle moving of main Minecraft directory

--- shared/scripts/init/Minecraft/restore-backup.sh
+++ shared/scripts/init/Minecraft/restore-backup.sh
@@ -2,9 +2,7 @@
 
 aws s3 cp "s3://${BACKUP_STORAGE_NAME}/${backup_version}.zip" Restore.zip
 
-if [ -d "${SERVER_MOUNT_LOCATION}/Minecraft/world" ]; then
-    mv "${SERVER_MOUNT_LOCATION}/Minecraft/world" "${SERVER_MOUNT_LOCATION}/Minecraft/world_old"
-fi
+
 
 mappings_file_name="${SHARED_DIR}/shared/data/minecraft_mappings.json"
 is_modded=$(cat $mappings_file_name | jq ".maps[] | select(.name == \"$MAP_NAME\") | .is_modded" | tr -d '"')
@@ -14,12 +12,21 @@ if [[ is_modded == "true" ]]; then
 else
 fi
 
+if [ -d "${SERVER_MOUNT_LOCATION}/${replace_folder}" ]; then
+    mv "${SERVER_MOUNT_LOCATION}/${replace_folder}" "${SERVER_MOUNT_LOCATION}/${replace_folder}_old"
+    mkdir -p "${SERVER_MOUNT_LOCATION}/${replace_folder}"
+fi
+
 if unzip -o Restore.zip -d "${SERVER_MOUNT_LOCATION}/Minecraft" ; then
         if [ -d "${SERVER_MOUNT_LOCATION}/${replace_folder}_old" ]; then
             rm -R "${SERVER_MOUNT_LOCATION}/${replace_folder}_old"
         fi
     else
-        if [ -d ${SERVER_MOUNT_LOCATION}/${replace_folder}_old ]; then
+        if [ -d "${SERVER_MOUNT_LOCATION}/${replace_folder}_old" ]; then
+            # Make room, we're moving it back
+            if [ -d "${SERVER_MOUNT_LOCATION}/${replace_folder}" ]; then
+                rm -R "${SERVER_MOUNT_LOCATION}/${replace_folder}"
+            fi
             mv "${SERVER_MOUNT_LOCATION}${replace_folder}_old" "${SERVER_MOUNT_LOCATION}/${replace_folder}"
         fi
         echo "Backup restore failed! Reverting to last."
