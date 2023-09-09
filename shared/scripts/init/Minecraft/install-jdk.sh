ed92b9035f100fea4c45bcae90a12a3fe65603f9
jkstenzel95 <jkstenzel95@outlook.com>
2023-09-09 16:12:31 -0700

    Update Pixelmon

--- shared/scripts/init/Minecraft/install-jdk.sh
+++ shared/scripts/init/Minecraft/install-jdk.sh
@@ -10,8 +10,8 @@ done
 
 if [ $version == 8 ]; then
     sudo yum update -y
-    sudo yum install java-1.8.0-openjdk -y
-elif [ $version == 17 ]; then
+    sudo yum install java-1.8.0-openjdk -y # openjdk-8-jdk on apt-get
+elif [ $version == 17 ]; then # try openjdk-17-jdk on apt-get
     sudo dnf -y install curl wget
     wget https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz
     tar xvf openjdk-17_linux-x64_bin.tar.gz
