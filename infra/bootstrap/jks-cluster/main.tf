9fc93b5ad46e69c3ed332efb3e643ebf17b9a1ba
Jacob Stenzel <jkstenzel95@outlook.com>
2024-11-10 12:51:15 -0800

    Add EBS CSI addon to cluster

--- infra/bootstrap/jks-cluster/main.tf
+++ infra/bootstrap/jks-cluster/main.tf
@@ -100,6 +100,32 @@ resource "aws_eks_cluster" "cluster" {
   ]
 }
 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${var.cluster_name}"
+  provider_url                  = replace(aws_eks_cluster.cluster.identity.0.oidc.0.issuer, "https://", "")
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = "${var.cluster_name}"
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.5.2-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
+  }
+}
+
 resource "aws_cloudwatch_log_group" "cluster_logs" {
   # The log group name format is /aws/eks/<cluster-name>/cluster
   # Reference: https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html
