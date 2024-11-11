d6407d7d6811d6e2df4fd44449f19df2df673928
Jacob Stenzel <jkstenzel95@outlook.com>
2024-11-10 17:27:52 -0800

    Fix EBS CSI version

--- infra/bootstrap/jks-cluster/main.tf
+++ infra/bootstrap/jks-cluster/main.tf
@@ -118,7 +118,7 @@ module "irsa-ebs-csi" {
 resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = "${var.cluster_name}"
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.5.2-eksbuild.1"
+  addon_version            = "v1.36.0-eksbuild.1"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
   tags = {
     "eks_addon" = "ebs-csi"
